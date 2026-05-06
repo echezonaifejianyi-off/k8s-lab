ca3be808690f1c9ddaf744e53158a95302ebaeab
Echezona Ifejianyi <echezonaifejianyi@gmail.com>
2026-05-06 09:10:14 +0100

    initial commit: terraform, ansible, manifests, go app

---
+++ app/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/healthz", healthzHandler)
+
+	log.Println("Server starting on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello from this side of the world!")
+}
+
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintln(w, "ok")
+}
